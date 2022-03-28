0903dfa5a5c98c8855149a3267143acd7f87e981
skbhoi2001 <soumyak.bhoi.2001@gmail.com>
2022-03-28 10:28:39 +0530

    P2

--- frontend/src/components/Products/EachProduct.jsx
+++ frontend/src/components/Products/EachProduct.jsx
@@ -1,9 +1,15 @@
 import { useEffect, useState } from "react";
 import { Link, useParams } from "react-router-dom";
 import StarOutlineIcon from '@mui/icons-material/StarOutline';
+import ErrorOutlineIcon from '@mui/icons-material/ErrorOutline';
+import ShoppingCartIcon from '@mui/icons-material/ShoppingCart';
+import BoltIcon from '@mui/icons-material/Bolt';
+import StarIcon from '@mui/icons-material/Star';
+import Styles from "../../css/Products/EachProduct.module.css"
 
 export default function EachProduct() {
   const [user, setUser] = useState({});
+  const [count,setCount] = useState(0)
   const pharms = useParams();
   useEffect(() => {
     if (pharms.id) {
@@ -15,15 +21,48 @@ export default function EachProduct() {
     }
   }, [pharms]);
   return (
-    <div>
-      <div>
+    <div className={Styles.container} >
+      <div className={Styles.containerImage} >
         <img src={user.image} alt="" />
       </div>
-      <div>
+      <div className={Styles.containerData}>
         <div>
-            <h2>{user.title}</h2>
-            <p>By <span>MuscleBlaze</span></p>
-            <h5>{user.rating} <StarOutlineIcon/> </h5>
+            <h3 className={Styles.byProduct}>{user.title}</h3>
+            <p className={Styles.byProduct}>By <span>MuscleBlaze</span></p>
+            <h5 className={Styles.rate}>{user.rating} <span><StarIcon className={Styles.star} /></span> </h5>
+            <button className={Styles.offer} >{`${user.discount}% OFF`}</button>
+            <div className={Styles.price} >
+                <h2>{`₹${user.price}`}</h2>
+                <p>₹{user.price+(user.price*(user.discount/100))}</p>
+                <p>Get HK Cash</p>
+            </div>
+            <div>
+              <p>3 interent free payments of ₹175</p>
+              <ErrorOutlineIcon/>
+            </div>
+            <div>
+              <p>+ Get 10% cashback up to ₹500</p>
+              <ErrorOutlineIcon/>
+            </div>
+            <div>
+              <div>
+                <button onClick={()=>setCount(count-1)} >-</button>
+                {count}
+                <button onClick={()=>setCount(count+1)} >+</button>  
+              </div>  
+              <button><ShoppingCartIcon/> ADD TO CART</button>
+              <button><BoltIcon/> QUICK BUY</button>
+            </div>  
+            <div>
+              <h5>WEIGHT</h5>
+              {
+                ["1.1","11","2.2"].map((r)=>{
+                  return(
+                    <button>{`${r} lb`}</button>
+                  )
+                })
+              }
+            </div>
         </div>
       </div>
     </div>
